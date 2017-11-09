3139c6a1b3f524c558117eb2c7dfa15ccde92e53
Niprichem <vbolshakoff@yandex.ru>
2017-11-09 22:29:39 +0600

    Update code with PEP8 standard.

--- coding_2_2.py
+++ coding_2_2.py
@@ -1,5 +1,3 @@
-#-*- coding: utf-8 -*-
-
 import os
 import chardet
 import json
@@ -9,8 +7,10 @@ import collections
 def get_files_gener(dir_name, extention):
     return (f_name for f_name in os.listdir(dir_name) if f_name.endswith('.{}'.format(extention)))
 
+
 def get_coding(data):
     return chardet.detect(data)['encoding']
+
 
 def read_file(name):
     with open(name, 'rb') as f:
@@ -18,20 +18,24 @@ def read_file(name):
         coding = get_coding(data)
         return data.decode(coding)
 
+
 def get_news_from_text_json(data):
     data_json = json.loads(data)['rss']['channel']['items']
     news_data = ' '.join('{} {} '.format(item['title'], item['description']) for item in data_json)
     return news_data
 
+
 def count_top_n_words(data, word_len, n_top_count):
     words = collections.Counter(word for word in data.split(' ') if len(word) > word_len)
     return words.most_common(n_top_count)
+
 
 def print_result(name, words_count):
     words = [word for word, count in words_count]
     words.reverse()
     words_str = ', '.join(words)
     print('{}: {}\n'.format(name, words_str))
+
 
 def main():
     news_path = u'news'
@@ -43,6 +47,7 @@ def main():
         news_data = get_news_from_text_json(data)
         result = count_top_n_words(news_data, word_len, n_top_count)
         print_result(name, result)
+
 
 if __name__ == '__main__':
     main()
